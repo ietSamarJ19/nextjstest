5765d4d35a6d5306f7d77fadb0aaedc28cafb16f
samarJ19 <samarjoshitech@gmail.com>
2025-06-19 15:14:32 +0530

    removed eslint error

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,9 @@ import React from 'react';
 import Link from 'next/link';
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu"
 import { cn } from "@/lib/utils"
 
@@ -32,7 +30,7 @@ const Navbar: React.FC = () => {
           <NavigationMenuList className="flex space-x-1">
             {navItems.map((item) => (
               <NavigationMenuItem key={item.href}>
-                <Link href={item.href} legacyBehavior passHref>
+                <Link href={item.href} passHref>
                   <NavigationMenuLink
                     className={cn(
                       "group inline-flex h-12 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
